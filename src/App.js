3766e18dce078a1c3f9fb6f775abd7e6cf9e5c88
Johnny Choi <idsopy@nate.com>
2022-05-24 22:39:03 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Box from './Box';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [size, setSize] = useState(100);
+
+  const createBoxStyle = ()=> {
+    return {
+      backgroundColor :'pink',
+      width: `${size}px`,
+      height: `${size}px`
+    };
+  };
+
+   return (
+    <div>
+      <input
+        type="number"
+        value={size}
+        onChange={(e)=> setSize(e.target.value)}
+      />
+      <Box createBoxStyle={createBoxStyle}/>
     </div>
   );
 }
